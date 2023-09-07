1ec00048d9e57cff7164e9dcd56e8536f0d21094
hanvid <hanvidlee@github.com>
2023-09-07 12:15:48 -0400

    dark mode and light mode functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import { colorModeContext, themeSettings, useMode } from "./theme";
+import {CssBaseline, ThemeProvider } from "@mui/material"
 
 function App() {
-  return (
+  const [theme, colorMode] = useMode();
+  
+  return (<colorModeContext.Provider value={colorMode}>
     <div className="app">
     </div>
+    </colorModeContext.Provider>
   );
 }
 
