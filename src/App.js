c21ddadcecdb284a9c36a19fe0cc3ff47552b444
hanvid <hanvidlee@github.com>
2023-09-13 11:14:34 -0400

    topbar search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { colorModeContext, themeSettings, useMode } from "./theme";
 import {CssBaseline, ThemeProvider } from "@mui/material"
-
+import Topbar from "./scenes/global/topbar"
 function App() {
   const [theme, colorMode] = useMode();
   
-  return (<colorModeContext.Provider value={colorMode}>
-    <div className="app">
-    </div>
+  return (
+  <colorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <main className="content">
+          <Topbar></Topbar>
+        </main>
+      </div>
+    </ThemeProvider>
     </colorModeContext.Provider>
   );
 }
--- src/scenes/global/topbar.jsx
+++ src/scenes/global/topbar.jsx
@@ -1,6 +1,22 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { colorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = () => {
-  return <div>topbar</div>
-}
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(colorModeContext)
 
-export default Topbar
+  return (<Box display="flex" justifyContent="space-between" p={2}>
+    <IconButton sx={{ display: "flex" }}></IconButton>
+  </Box>);
+};
+
+export default Topbar;
